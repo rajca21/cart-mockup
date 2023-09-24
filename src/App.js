a70da9629095d7ea4cf8aa42af5e1f10638ad5b4
rajca21 <2nikolar1@gmail.com>
2023-09-24 16:17:03 +0200

    update-for-netlify-deploy

---
+++ src/App.js
@@ -0,0 +1,31 @@
+// react imports
+import React from 'react';
+
+// context
+import { useGlobalContext } from './context';
+
+// components
+import Navbar from './Navbar';
+import CartContainer from './CartContainer';
+
+// items
+
+function App() {
+  const { loading } = useGlobalContext();
+
+  if (loading) {
+    return (
+      <div className='loading'>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+  return (
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
+}
+
+export default App;
